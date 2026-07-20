a5988be89f5cd9954d3f29b6c703f70242bbbea3
YuryEreshchenko <yuryere@gmail.com>
2026-07-20 15:12:22 -0400

    Test LCD

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -7,6 +7,12 @@
 #include "leds.h"
 #include "comm_bridge.h"
 
+// ===== TEST ECRAN TEMPORAIRE (a retirer apres validation) =====
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+static Adafruit_ILI9341 ecranTest = Adafruit_ILI9341(10, 9, 8);  // CS, DC, RST
+
 // ======================== LEDs ========================
 
 static void rpc_mode_led1(int mode) { Leds_DefinirMode(1, mode); }
@@ -15,6 +21,16 @@ static void rpc_mode_led2(int mode) { Leds_DefinirMode(2, mode); }
 // ======================== Setup ========================
 
 void setup() {
+    
+    // ===== TEST ECRAN TEMPORAIRE =====
+    ecranTest.begin();
+    ecranTest.setRotation(1);
+    ecranTest.fillScreen(ILI9341_BLUE);
+    ecranTest.setTextColor(ILI9341_WHITE);
+    ecranTest.setTextSize(4);
+    ecranTest.setCursor(40, 100);
+    ecranTest.print("TankETS");
+    
     Serial.begin(9600);
     Wire1.begin();
     delay(500);
